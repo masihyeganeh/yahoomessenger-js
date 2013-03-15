e8caa11125e23b23fc2219e406892a598c24447c
Masih Yeganeh <goodboy.php@gmail.com>
2013-03-15 13:43:38 +0330

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# YahooMessenger.js
+
+Yahoo! Messenger in node.js
+
+This is an alpha release.
---
+++ index.js
@@ -0,0 +1,8 @@
+
+/*!
+ * YahooMessenger
+ * Copyright(c) 2013 Masih Yeganeh <masihyeganeh@outlook.com>
+ * MIT Licensed
+ */
+
+module.exports = require('./lib/Messenger');
